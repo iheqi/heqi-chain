7b2158c70abf98c83167b3440eeb30b94bf99534
rubbish-man <993284758@qq.com>
2018-12-14 21:54:08 +0800

    校验区块

--- src/blockchain.js
+++ src/blockchain.js
@@ -12,10 +12,9 @@ const initBlock = {
 class Blockchain {
   constructor () {
     this.blockchain = [initBlock];
-    this.data = [];
+    // this.data = [];
     this.difficulty = 3;
-
-    const hash = this.computedHash(0, '0', Date.now(), 'Hello heqi-chain', 1);
+    // const hash = this.computedHash(0, '0', Date.now(), 'Hello heqi-chain!', 1);
   }
 
   getLastBlock() {
@@ -23,25 +22,48 @@ class Blockchain {
   }
 
   mine() {
+    const newBlock = this.generateNewBlock();
+
+    if (this.isVaildBlock(newBlock) && this.isVaildChain()) {
+      this.blockchain.push(newBlock);
+    } else {
+      console.log('error, invalid block.');
+    }
+  }
+
+  generateNewBlock() {
     let nonce = 0;
     const index = this.blockchain.length;
     const data = 'Hello heqi-chain!';
     const prevHash = this.getLastBlock().hash;
-    // const prevHash = '0'
     const timestamp = Date.now();
-
     let hash = this.computedHash(index, prevHash, timestamp, data, nonce);
 
     while (hash.substring(0, this.difficulty) !== '0'.repeat(this.difficulty)) {
       nonce++;
       hash = this.computedHash(index, prevHash, timestamp, data, nonce);
     }
+    console.log({
+      hash,
+      nonce,
+      timestamp,
+      prevHash,
+      data,
+      index
+    });
 
-    console.log(hash, nonce, timestamp);
+    return {
+      hash,
+      nonce,
+      timestamp,
+      prevHash,
+      data,
+      index
+    }
   }
 
-  generateNewBlock() {
-
+  computedHashForBlock({index, prevHash, timestamp, data, nonce}) {
+    return this.computedHash(index, prevHash, timestamp, data, nonce);
   }
 
   computedHash(index, prevHash, timestamp, data, nonce) {
@@ -50,14 +72,38 @@ class Blockchain {
                  .digest('hex');
   }
 
-  isVaildBlock() {
-
+  isVaildBlock(newBlock, lastBlock=this.getLastBlock()) { // 这里还要验证？fuck，本来就是这样来的
+    if (newBlock.index !== lastBlock.index + 1 ||
+        newBlock.timestamp <= lastBlock.timestamp ||
+        newBlock.prevHash !== lastBlock.hash ||
+        newBlock.hash.substring(0, this.difficulty) !== '0'.repeat(this.difficulty) ||
+        newBlock.hash !== this.computedHashForBlock(newBlock)
+    ) {
+      return false;
+    }
+    return true;
   }
 
-  isVaildChain() {
+  isVaildChain(chain=this.blockchain) {
+    for (let i = 2; i < chain.length; i++) {
+      const currentBlock = chain[i];
+      const previousBlock = chain[i - 1];
 
+      if (!this.isVaildBlock(currentBlock, previousBlock)) {
+        return false;
+      }
+    }
+
+    if (JSON.stringify(chain[0]) !== JSON.stringify(initBlock)) {
+      return false;
+    }
+    return true;
   }
 }
 
 let bc = new Blockchain();
+bc.mine();
+bc.mine();
+bc.blockchain[1].index = 100;
+bc.mine();
 bc.mine();
