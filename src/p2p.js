c80146cbe76876e12472c095e5b794b9df51e7d1
rubbish-man <993284758@qq.com>
2018-12-16 20:39:39 +0800

    sayhi逻辑

--- src/p2p.js
+++ src/p2p.js
@@ -5,6 +5,7 @@ class P2p {
     this.peers = [];
     this.seed = { port: 8001, address: 'localhost' };
     this.udp = dgram.createSocket('udp4');
+    this.remote = {};
   }
 
   init() {
@@ -13,12 +14,12 @@ class P2p {
   }
 
   bindP2p() {
-    this.udp.on('message', (msg, rinfo) => {
-      console.log(`服务器收到：${msg} 来自 ${rinfo.port}`);
+    this.udp.on('message', (msg, remote) => {
+      console.log(`${remote.address}:${remote.port}: ${msg}`);
       const action = JSON.parse(msg);
 
       if (action.type) {
-        this.dispatch(action, rinfo);
+        this.dispatch(action, remote);  
       }
     });
 
@@ -36,7 +37,8 @@ class P2p {
 
     if (port !== 8001) {
       this.send({
-        type: 'newpeer'
+        type: 'newpeer',
+        data: 'Hi,种子,我是新节点！'
       }, this.seed.port, this.seed.address);
     }
   }
@@ -53,13 +55,68 @@ class P2p {
 
   dispatch(action, remote) {
     switch (action.type) {
-      case 'newpeer':
-        console.log('你好，新朋友！');
+      case 'newpeer': {
+        this.send({
+          type: 'remoteAddress',
+          data: remote
+        }, remote.port, remote.address);
+
+        this.send({
+          type: 'peerlist',
+          data: this.peers
+        }, remote.port, remote.address);
+
+        this.boardcast({
+          type: 'sayhi',
+          data: remote
+        });
+
+        this.peers.push(remote);
+        break;
+      }
+      case 'remoteAddress': { // 新节点得到种子节点告诉的ip和端口
+        this.remote = action.data;
+        break;
+      }
+      case 'peerlist': {
+        const newPeers = action.data;
+        this.addPeers(newPeers);
+        break;
+      }
+      case 'sayhi': {
+        const remote = action.data;
+        this.peers.push(remote);
+        // console.log(`我是${this.remote.address}:${this.remote.port},欢迎新节点!`);
+        this.send({
+          type: 'hi'
+        }, remote.port, remote.address);
+        break;
+      }  
+      case 'hi':
+        console.log(`${remote.address}:${remote.port}: Hi`);
         break;
       default:
         console.log('无此action.type!');
     }
   }  
+
+  addPeers(newPeers) {
+    newPeers.forEach(peer => {
+      if (!this.peers.find(val => this.isEqualPeer(peer, val))) {
+        this.peers.push(peer);
+      }
+    });
+  }
+
+  isEqualPeer(p1, p2) {
+    return p1.address === p2.address && p2.port === p2.port;
+  }
+
+  boardcast(action) {
+    this.peers.forEach(peer => {
+      this.send(action, peer.port, peer.address);
+    });
+  }
 }
 
 
